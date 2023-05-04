cd0f84d315ea2456f853d2eca3a858a9170acc60
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-05-04 16:26:09 +0200

    Render default Lists and adapt List output of Reminder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,40 @@
 <main class="flex overflow-hidden h-screen">
-    <aside class="w-80 bg-gray-200 border-r h-full p-4">Sidebar</aside>
-    <section class="flex-1 p-4 overflow-y-scroll overflow-x-hidden grid gap-8 justify-start content-start">
-        <h1 class="text-3xl font-bold">List title</h1>
-
-        <ul class="grid gap-4 max-w-screen-sm">
-            {#each reminders as {title, isDone}, i (title + i)}
+    <aside class="w-80 bg-gray-100 border-r h-full p-4">
+        <ul class="grid gap-4">
+            {#each lists as list}
                 <li>
-                    <div class="flex gap-4 items-center">
-                        <input type="checkbox" checked="{isDone}"
-                               on:click={isDone ? false : !isDone ? true : true }/> {title}
+                    <div class="flex justify-between gap-4 items-center p-4 rounded-2xl cursor-pointer transition-all {list === activeList ? 'bg-gray-200' : 'bg-transparent'}"
+                         on:mousedown={() => activeList = list}>
+                        <h2 class="font-bold">{list}</h2>
+                        <div>{reminders.filter(reminder => reminder.assignedLists.includes(list)).length}</div>
                     </div>
                 </li>
             {/each}
         </ul>
+    </aside>
 
-        <button on:click={addReminder}>Add reminder</button>
+    <section class="flex-1 p-4 overflow-y-scroll overflow-x-hidden grid gap-8 justify-start content-start">
+        <header>
+            <button on:click={addReminder}>+</button>
+        </header>
+
+        <h1 class="text-3xl font-bold">{activeList}</h1>
+
+        <ul class="grid gap-4 max-w-screen-sm">
+            {#each reminders as {title, isDone, assignedLists}, i (title + i)}
+                {#if assignedLists.includes(activeList)}
+                    <li>
+                        <div class="flex gap-4 items-center cursor-pointer transition-all {isDone ? 'line-through' : ''}"
+                             on:mousedown={() => markAsDone(title)}>
+                            <span class="h-4 w-4 flex items-center justify-center rounded-full border">
+                                <span class="h-3 w-3 bg-blue-400 rounded-full transition-all {isDone ? 'scale-100' : 'scale-0'}"></span>
+                            </span>
+                            <h2 class="{isDone ? 'opacity-30' : 'opacity-100'}"> {title}</h2>
+                        </div>
+                    </li>
+                {/if}
+            {/each}
+        </ul>
     </section>
 </main>
 
@@ -22,16 +42,23 @@
     interface Reminder {
         title: string
         isDone: boolean
+        assignedLists: string[]
     }
+
+    let lists = ['Today', 'Planned', 'All', 'Marked', 'Done']
+
+    let activeList = 'Today'
 
     let reminders: Reminder[] = [
         {
             title: 'A first reminder',
-            isDone: false
+            isDone: false,
+            assignedLists: ['Today', 'All']
         },
         {
             title: 'Second reminder',
-            isDone: true
+            isDone: true,
+            assignedLists: ['Today', 'All', "Done"]
         }
     ]
 
@@ -40,8 +67,22 @@
             ...reminders,
             {
                 title: 'Newly added reminder',
-                isDone: false
+                isDone: false,
+                assignedLists: ['Today', 'All']
             }
         ]
+    }
+
+    function markAsDone(reminderTitle: string) {
+        reminders = reminders.map(currReminder => {
+            if (currReminder.title === reminderTitle) {
+                return {
+                    ...currReminder,
+                    isDone: !currReminder.isDone
+                }
+            } else {
+                return currReminder
+            }
+        })
     }
 </script>
