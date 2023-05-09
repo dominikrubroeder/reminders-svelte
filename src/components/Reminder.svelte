a2a473382b338e1b0f2086d0bff1545329e3bd61
Dominik Rubröder <dominik.rubroeder@mediawave.de>
2023-05-09 17:55:17 +0200

    Render Reminder Tags in Sidebar and within Reminder View

--- src/components/Reminder.svelte
+++ src/components/Reminder.svelte
@@ -1,40 +1,38 @@
 <script lang="ts">
-    export interface IReminder {
-        title: string;
-        isDone: boolean;
-        assignedCategories: string[];
-        assignedLists: string[];
-        assignedTags: string[];
-    }
+	export interface IReminder {
+		title: string;
+		isDone: boolean;
+		assignedCategories: string[];
+		assignedLists: string[];
+		assignedTags: string[];
+	}
 
-    export let title: 'A reminder'
-    export let isDone: false
-    export let assignedCategories: []
-    export let assignedLists: []
-    export let assignedTags: []
-
-    let isOpen = false
+	export let title = 'A reminder';
+	export let isDone = false;
+	export let assignedCategories = [];
+	export let assignedLists = [];
+	export let assignedTags = [];
 </script>
 
-<div
-        class="grid gap-2"
-        on:mousedown={() => isOpen = !isOpen}
->
-    <header class="flex cursor-pointer items-center gap-4 transition-all {isDone
-		? 'line-through'
-		: ''}">
-		<span class="flex h-4 w-4 items-center justify-center rounded-full border" on:mousedown={() => markAsDone()}>
+<div class="grid gap-2">
+	<header
+		class="flex cursor-pointer items-center gap-4 transition-all {isDone ? 'line-through' : ''}"
+	>
 		<span
-                class="h-3 w-3 rounded-full bg-blue-400 transition-all {isDone
-				? 'scale-100'
-				: 'scale-0'}"></span>
-	</span>
-        <h2 class={isDone ? 'opacity-30' : 'opacity-100'}>{title}</h2>
-    </header>
+			class="flex h-4 w-4 items-center justify-center rounded-full border"
+			on:mousedown={() => markAsDone()}
+		>
+			<span
+				class="h-3 w-3 rounded-full bg-blue-400 transition-all {isDone ? 'scale-100' : 'scale-0'}"></span>
+		</span>
+		<h2 class={isDone ? 'opacity-30' : 'opacity-100'}>{title}</h2>
+	</header>
 
-    {#if isOpen}
-        <div class="pl-8">
-            Show isOpen content
-        </div>
-    {/if}
+	{#if assignedTags.length >= 1}
+		<ul class="flex gap-2 pl-8">
+			{#each assignedTags as assignedTag}
+				<li class="rounded bg-gray-100 px-3 py-2 text-xs">#{assignedTag}</li>
+			{/each}
+		</ul>
+	{/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,198 +1,207 @@
 <script lang="ts">
-    import Reminder from "../components/Reminder.svelte";
-    import type {IReminder} from "../components/Reminder.svelte";
+	import Reminder from '../components/Reminder.svelte';
+	import type { IReminder } from '../components/Reminder.svelte';
 
-    interface List {
-        title: string;
-        backgroundColor: string;
-        textColor: string;
-    }
+	interface List {
+		title: string;
+		backgroundColor: string;
+		textColor: string;
+	}
 
-    interface Category {
-        title: string;
-        backgroundColor: string;
-        textColor: string;
-    }
+	interface Category {
+		title: string;
+		backgroundColor: string;
+		textColor: string;
+	}
 
-    interface ActiveList {
-        title: string;
-        type: 'List' | 'Tag' | 'Category';
-    }
+	interface ActiveList {
+		title: string;
+		type: 'List' | 'Tag' | 'Category';
+	}
 
-    let categories: Category[] = [
-        {
-            title: 'Today',
-            backgroundColor: 'bg-blue-400',
-            textColor: 'text-white'
-        },
-        {title: 'Planned', backgroundColor: 'bg-red-400', textColor: 'text-white'},
-        {
-            title: 'All',
-            backgroundColor: 'bg-gray-700',
-            textColor: 'text-white'
-        },
-        {title: 'Marked', backgroundColor: 'bg-orange-400', textColor: 'text-white'},
-        {
-            title: 'Done',
-            backgroundColor: 'bg-slate-600',
-            textColor: 'text-white'
-        }
-    ];
+	let categories: Category[] = [
+		{
+			title: 'Today',
+			backgroundColor: 'bg-blue-400',
+			textColor: 'text-white'
+		},
+		{ title: 'Planned', backgroundColor: 'bg-red-400', textColor: 'text-white' },
+		{
+			title: 'All',
+			backgroundColor: 'bg-gray-700',
+			textColor: 'text-white'
+		},
+		{ title: 'Marked', backgroundColor: 'bg-orange-400', textColor: 'text-white' },
+		{
+			title: 'Done',
+			backgroundColor: 'bg-slate-600',
+			textColor: 'text-white'
+		}
+	];
 
-    let lists: List[] = [
-        {
-            title: 'Reminders',
-            backgroundColor: 'bg-red-400',
-            textColor: 'text-white'
-        },
-        {title: 'Another list', backgroundColor: 'bg-red-400', textColor: 'text-white'}
-    ];
+	let lists: List[] = [
+		{
+			title: 'Reminders',
+			backgroundColor: 'bg-red-400',
+			textColor: 'text-white'
+		},
+		{ title: 'Another list', backgroundColor: 'bg-red-400', textColor: 'text-white' }
+	];
 
-    let tags = ['All tags'];
+	let activeList: ActiveList = { title: 'Today', type: 'Category' };
+	let searchValue = '';
 
-    let activeList: ActiveList = {title: 'Today', type: 'Category'};
-    let searchValue = '';
+	let reminders: IReminder[] = [
+		{
+			title: 'A first reminder',
+			isDone: false,
+			assignedCategories: ['All'],
+			assignedLists: [],
+			assignedTags: ['development', 'svelte']
+		},
+		{
+			title: 'Second reminder',
+			isDone: true,
+			assignedCategories: ['All'],
+			assignedLists: [],
+			assignedTags: ['food']
+		}
+	];
 
-    let reminders: IReminder[] = [
-        {
-            title: 'A first reminder',
-            isDone: false,
-            assignedCategories: ['All'],
-            assignedLists: [],
-            assignedTags: []
-        },
-        {
-            title: 'Second reminder',
-            isDone: true,
-            assignedCategories: ['All'],
-            assignedLists: [],
-            assignedTags: []
-        }
-    ];
+	$: tags = ['All tags', ...reminders.map(reminder => reminder.assignedTags).flat()]
 
-    function addReminder() {
-        reminders = [
-            ...reminders,
-            {
-                title: 'Newly added reminder',
-                isDone: false,
-                assignedCategories: ['All'],
-                assignedLists: [],
-                assignedTags: []
-            }
-        ];
-    }
+	function addReminder() {
+		reminders = [
+			...reminders,
+			{
+				title: 'Newly added reminder',
+				isDone: false,
+				assignedCategories: ['All'],
+				assignedLists: [],
+				assignedTags: []
+			}
+		];
+	}
 
-    function markAsDone(reminderTitle: string) {
-        reminders = reminders.map((currReminder) => {
-            if (currReminder.title === reminderTitle) {
-                return {
-                    ...currReminder,
-                    isDone: !currReminder.isDone
-                };
-            } else {
-                return currReminder;
-            }
-        });
-    }
+	function markAsDone(reminderTitle: string) {
+		reminders = reminders.map((currReminder) => {
+			if (currReminder.title === reminderTitle) {
+				return {
+					...currReminder,
+					isDone: !currReminder.isDone
+				};
+			} else {
+				return currReminder;
+			}
+		});
+	}
 </script>
 
 <main class="flex h-screen overflow-hidden">
-    <aside class="grid h-full w-80 content-start gap-4 border-r bg-gray-100 p-4">
-        <section>
-            <input
-                    placeholder="Search"
-                    bind:value={searchValue}
-                    class="w-full rounded border bg-gray-200 px-3 py-1 text-sm"
-            />
-        </section>
+	<aside class="grid h-full w-80 content-start gap-8 border-r bg-gray-100 p-4">
+		<section class="grid gap-4">
+			<section>
+				<input
+						placeholder="Search"
+						bind:value={searchValue}
+						class="w-full rounded border bg-gray-200 px-3 py-1 text-sm"
+				/>
+			</section>
 
-        <section>
-            <ul class="grid grid-cols-2 gap-2">
-                {#each categories as category}
-                    <li>
-                        <div
-                                class="grid cursor-pointer gap-1 rounded-2xl p-4 transition-all {category.title ===
+			<section>
+				<ul class="grid grid-cols-2 gap-2">
+					{#each categories as category}
+						<li>
+							<div
+									class="grid cursor-pointer gap-1 rounded-2xl p-4 transition-all {category.title ===
 							activeList.title
-								? 'bg-blue-400'
+								? category.backgroundColor
 								: 'bg-gray-200'}"
-                                on:mousedown={() => (activeList = { title: category.title, type: 'Category' })}
-                        >
-                            <div class="flex items-center justify-between gap-4">
-                                <span class="h-5 w-5 {category.backgroundColor} shrink-0 rounded-full"></span>
-                                <div>
-                                    {reminders.filter((reminder) => reminder.assignedLists.includes(category)).length}
-                                </div>
-                            </div>
-                            <h2 class="font-bold">{category.title}</h2>
-                        </div>
-                    </li>
-                {/each}
-            </ul>
-        </section>
+									on:mousedown={() => (activeList = { title: category.title, type: 'Category' })}
+							>
+								<div class="flex items-center justify-between gap-4">
+								<span
+										class="h-5 w-5 {category.title === activeList.title
+										? 'bg-white'
+										: category.backgroundColor} shrink-0 rounded-full"></span>
+									<div class={category.title === activeList.title ? 'text-white' : ''}>
+										{reminders.filter((reminder) => reminder.assignedLists.includes(category)).length}
+									</div>
+								</div>
+								<h2 class="font-bold {category.title === activeList.title ? 'text-white' : ''}">
+									{category.title}
+								</h2>
+							</div>
+						</li>
+					{/each}
+				</ul>
+			</section>
+		</section>
 
-        <section class="grid gap-2">
-            <h2 class="text-xs">Meine Listen</h2>
-            <ul class="grid">
-                {#each lists as list}
-                    <li>
-                        <div
-                                class="flex cursor-pointer items-center justify-between gap-4 rounded-2xl p-4 transition-all {list.title ===
+		<section class="grid gap-2">
+			<h2 class="text-xs">Meine Listen</h2>
+			<ul class="grid">
+				{#each lists as list}
+					<li>
+						<div
+							class="flex cursor-pointer items-center justify-between gap-4 rounded-2xl px-4 py-3 {list.title ===
 							activeList.title
 								? 'bg-gray-200'
 								: 'bg-transparent'}"
-                                on:mousedown={() => (activeList = { title: list.title, type: 'List' })}
-                        >
-                            <div class="flex items-center gap-2">
+							on:mousedown={() => (activeList = { title: list.title, type: 'List' })}
+						>
+							<div class="flex items-center gap-2">
 								<span
-                                        class="h-5 w-5 shrink-0 rounded-full {list.backgroundColor} {list.textColor}"></span>
-                                <h2 class="flex-1 font-bold">{list.title}</h2>
-                            </div>
-                            <div>
-                                {reminders.filter((reminder) => reminder.assignedLists.includes(list)).length}
-                            </div>
-                        </div>
-                    </li>
-                {/each}
-            </ul>
-        </section>
+									class="h-5 w-5 shrink-0 rounded-full {list.backgroundColor} {list.textColor}"></span>
+								<h2 class="flex-1 font-bold">{list.title}</h2>
+							</div>
+							<div>
+								{reminders.filter((reminder) => reminder.assignedLists.includes(list)).length}
+							</div>
+						</div>
+					</li>
+				{/each}
+			</ul>
+		</section>
 
-        <section class="grid gap-2">
-            <h2 class="text-xs">Tags</h2>
-            <ul class="flex gap-1">
-                {#each tags as tag}
-                    <li>
-                        <div
-                                class="cursor-pointer rounded px-2.5 py-1.5 text-sm transition-all {tag.title ===
-							activeList.title
+		<section class="grid gap-2">
+			<h2 class="text-xs">Tags</h2>
+			<ul class="flex gap-1 flex-wrap">
+				{#each tags as tag}
+					<li>
+						<div
+							class="cursor-pointer rounded px-2.5 py-1.5 text-sm {tag.title === activeList.title
 								? 'bg-blue-400'
 								: 'bg-gray-200'}"
-                                on:mousedown={() => (activeList = { title: tag, type: 'Tag' })}
-                        >
-                            {tag}
-                        </div>
-                    </li>
-                {/each}
-            </ul>
-        </section>
-    </aside>
+							on:mousedown={() => (activeList = { title: tag, type: 'Tag' })}
+						>
+							{tag}
+						</div>
+					</li>
+				{/each}
+			</ul>
+		</section>
+	</aside>
 
-    <section class="grid flex-1 content-start gap-8 overflow-x-hidden overflow-y-scroll p-4">
-        <header>
-            <button on:click={addReminder}>+</button>
-        </header>
+	<section class="grid flex-1 content-start gap-8 overflow-x-hidden overflow-y-scroll p-4">
+		<header>
+			<button on:click={addReminder}>+</button>
+		</header>
 
-        <h1 class="text-3xl font-bold">{activeList.title}</h1>
+		<h1 class="text-3xl font-bold flex gap-0">
+			{activeList.type === 'Tag' ? '#' : ''}
+			{activeList.title}
+		</h1>
 
-        <ul class="grid gap-4">
-            {#each reminders as reminder, i (reminder.title + i)}
-                {#if (reminder.assignedCategories.includes(activeList.title) || reminder.assignedLists.includes(activeList.title) || reminder.assignedTags.includes(activeList.title)) && reminder.title.includes(searchValue)}
-                    <li>
-                        <Reminder {...reminder} on:mousedown/>
-                        <hr class="ml-8 mt-4"/>
-                    </li>
-                {/if}
-            {/each}
-        </ul>
-    </section>
+		<ul class="grid gap-4">
+			{#each reminders as reminder, i (reminder.title + i)}
+				{#if (reminder.assignedCategories.includes(activeList.title) || reminder.assignedLists.includes(activeList.title) || reminder.assignedTags.includes(activeList.title)) && reminder.title.includes(searchValue)}
+					<li>
+						<Reminder {...reminder} on:mousedown />
+						<hr class="ml-8 mt-4" />
+					</li>
+				{/if}
+			{/each}
+		</ul>
+	</section>
 </main>
