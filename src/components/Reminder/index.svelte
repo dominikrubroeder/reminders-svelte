715e0239eaaad92982b868b3f9ba62e9b8d82cee
Dominik Rubröder <dominik.rubroeder@mediawave.de>
2023-05-10 15:49:32 +0200

    Init EditMode in Reminders View

--- src/components/Reminder/index.svelte
+++ src/components/Reminder/index.svelte
@@ -11,11 +11,13 @@
 	}
 
 	export let markAsDone: (title: string) => void
+	let isEditMode = false
+	let newTag = ""
 </script>
 
 <div class="grid gap-2">
 	<header
-		class="flex cursor-pointer items-center gap-4"
+		class="flex cursor-pointer items-center gap-4 z-40 relative"
 		on:mousedown={markAsDone}
 	>
 		<span
@@ -23,9 +25,28 @@
 			<span
 				class="h-3 w-3 rounded-full bg-blue-400 transition-all {reminder.isDone ? 'scale-100' : 'scale-0'}"></span>
 		</span>
-		<div class="flex gap-1">
-			{#if reminder.priority}<span>{reminder.priority}</span>{/if}
-			<h2 class="{reminder.isDone ? 'text-gray-400 line-through' : 'opacity-100 text-gray-900'}">{reminder.title}</h2>
+
+		<div class="flex items-center justify-between gap-4 w-full">
+			<div class="flex gap-1">
+				{#if reminder.priority}<span>{reminder.priority}</span>{/if}
+
+				<div class="relative">
+					{#if isEditMode}
+						<input bind:value={reminder.title} class="bg-transparent outline-none w-full line-through" autofocus />
+						{:else}
+						<h2 class="{reminder.isDone ? 'text-gray-400 line-through' : 'opacity-100 text-gray-900'}">{reminder.title}</h2>
+					{/if}
+				</div>
+
+			</div>
+
+			<button class="flex items-center z-50" on:click={() => isEditMode = !isEditMode}>
+				{#if isEditMode}
+					<span class="text-xs">Done</span>
+					{:else}
+					<span>...</span>
+				{/if}
+			</button>
 		</div>
 	</header>
 
@@ -36,10 +57,27 @@
 	{/if}
 
 	{#if reminder.assignedTags.length >= 1}
-		<ul class="flex gap-2 pl-8">
+		<ul class="flex gap-2 pl-8 flex-wrap items-center">
 			{#each reminder.assignedTags as assignedTag}
-				<li class="rounded bg-gray-100 px-3 py-2 text-xs">#{assignedTag}</li>
+				<li class="rounded bg-gray-100 px-3 py-2 text-xs">
+					{#if isEditMode}
+						<input bind:value={assignedTag} class="w-auto bg-transparent" />
+						{:else}
+						#{assignedTag}
+					{/if}
+				</li>
 			{/each}
+
+			{#if isEditMode}
+				<li class="text-xs">
+					<form on:submit|preventDefault={() => {
+						reminder.assignedTags = [...reminder.assignedTags, newTag.replaceAll(' ', '')]
+						newTag = ""
+					}}>
+						<input placeholder="Add Tag" bind:value={newTag} />
+					</form>
+				</li>
+			{/if}
 		</ul>
 	{/if}
 </div>
